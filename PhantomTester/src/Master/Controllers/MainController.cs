9020f6b5f4de626a4637b33b4f545314a7b6503a
Michal <mich96k5@edu.easj.dk>
2017-05-01 11:24:39 +0200

    POST action method added

---
+++ PhantomTester/src/Master/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Master.Controllers
+{
+    public class MainController : Controller
+    {
+        /// <summary>
+        /// POST method for receiving and proccessing the command
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>IActionResult</returns>
+        [HttpPost]
+        [Route("main")]
+        public IActionResult Post([FromBody] JArray command)
+        {
+            return Ok(command);
+        }
+    }
+}
