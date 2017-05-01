dc1fb7e4f6a497543d5a10ed23a4bf408cf45828
zeph3ron <zeph3ron@gmail.com>
2017-05-01 10:54:48 +0200

    Added some test models

---
+++ PhantomTester/src/Model/WorkerCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [DataContract]
+    public abstract class WorkerCommand
+    {
+        [DataMember]
+        protected readonly string Command;
+        [DataMember]
+        protected readonly string[] Parameters;
+        protected WorkerCommand(string[] parameters)
+        {
+            Parameters = parameters;
+            Command = GetType().Name;
+        }
+        //public abstract void Execute(PhantomJSDriver driver);
+    }
+}
---
+++ PhantomTester/src/Model/WorkerRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Model
+{
+
+    public class WorkerRequest
+    {
+        public string Name { get; set; }
+        public string RootUrl { get; set; }
+        public ResponseType ResponseType { get; }
+        public List<WorkerCommand> Commands { get; }
+
+        public WorkerRequest(string requestName, string rootUrl, ResponseType responseType)
+        {
+            Name = requestName;
+            RootUrl = rootUrl;
+            ResponseType = responseType;
+            Commands = new List<WorkerCommand>();
+        }
+
+        public void AddCommand(WorkerCommand command)
+        {
+            Commands.Add(command);
+        }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ResponseType
+    {
+        Assertion,
+        Html
+    }
+}
