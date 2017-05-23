af8e5aa692e231675b720061628bbd3de6715eb8
Michal <mich96k5@edu.easj.dk>
2017-05-23 13:59:55 +0200

    reset project added

---
+++ PhantomTester/src/Reset/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reset.Models;
+
+namespace Reset
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (PhantomTesterContext db = new PhantomTesterContext())
+                {
+                    foreach (Token token in db.Tokens)
+                    {
+                        token.Usages = 0;
+                        Console.WriteLine(token.Id + " Token usages reset");
+                    }
+                    db.SaveChanges();
+                    Console.WriteLine("----- Changes to the database were saved -----");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("///////////////// EXCEPTION trown: " + e.Message);
+            }
+        }
+    }
+}
