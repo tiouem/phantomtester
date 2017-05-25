3fdf4cb04cb64a79c63bad6bc7fc774893ec1570
TIOUEM\tomas <tomas.vit92@gmail.com>
2017-05-25 21:23:42 +0200

    App insights for Master

--- PhantomTester/src/Master/Program.cs
+++ PhantomTester/src/Master/Program.cs
@@ -17,6 +17,7 @@ namespace Master
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
